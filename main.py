b9f8383a5b136ecec49ce3b46e8752c97aeff392
SuperTal3 <talkelley3@outlook.com>
2019-09-23 15:50:47 -0500

    Updated tons of stuff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import csv
+import pandas as pd
 
+def main():
+    intro()
+
+#Runs main menu and prints intro to program
 def intro():
     choose = False
     usrChoice = 0
@@ -19,20 +23,71 @@ def intro():
         elif(usrChoice == 3):
             addItem()
         elif(usrChoice == 4):
-            print("Exiting Program")
+            print("Exiting Program...")
+            choose = True
         else:
             print("Please enter a valid input")
 
+#Lists contents of CSV File
 def listSBL():
-    print("Listing Products...")
+    print("Listing Products... \n" )
+    df = pd.read_csv("sbl.csv")
+    print(df)
+    print("\n")
 
+#Called to edit or Remove an Item to the CSV file
 def editRM():
-    #Coolish
-    print("Cool")
-
-def addItem():
-    #Cool
-    print("Cool")
+    listSBL()
+    df = pd.read_csv("sbl.csv")
+    print("Select row to edit by Product name: ")
     
-def main():
-    intro()
+    print(df)
+#Called to add an Item to the CSV file
+def addItem():
+    #Initialize local variables
+    usrName = ""
+    usrPrice = 0
+    usrValue = 0
+    usrLink = ""
+    usrConf = ""
+    #List the contents of the csv
+    listSBL()
+    print("Add item: \n" )
+    #Read CSV to Pandas DataFrame
+    df =  pd.read_csv("sbl.csv")
+    #Create dictionary to hold information
+    newRow = {"Name" : "Null", "Price" : 0, "Value" : 0, "Link" : "http://talkelley3.com/" }
+    #Get user input for each category
+    print("Please enter information for the new row below:")
+    usrName = input("Enter the Name of the product: ")
+    usrPrice = int(input("Enter the price of the product: $"))
+    usrValue = int(input("Enter the value on a scale of 1-10:"))
+    usrLink = input("Enter the link to the item: ")
+    #Failsafe to not allow null fields
+    if(usrName != "" or usrPrice != 0 or usrValue != "" or usrLink != ""):
+        #Apply changes to dictionary
+        print("Applying changes to dictionary...")
+        newRow["Name"] = usrName
+        newRow["Price"] = usrPrice
+        newRow["Value"] = usrValue
+        newRow["Link"] = usrLink
+        print(newRow)
+        #Append to csv file and re-display to confirm
+        df = df.append(newRow, ignore_index = True)
+        print(df)
+        print("\n")
+        usrConf = input("Confirm addition (Y/N): ")
+        if(usrConf == "Y" or usrConf == "y"):
+            print("Adding Entry...")
+            df.to_csv('sbl.csv', mode='w', header=True, index=False)
+        elif(usrConf == "N" or usrConf == "n"):
+            print("Addition Cancelled...")
+        else:
+            print("Failed confirmation. exiting")
+    else:
+        print("Failure to enter all fields.  Exiting...")
+
+
+#Statement to run main function if this is the main program and not a module    
+if __name__ == '__main__':
+    main()
